fff9f223ec9529b7c21511e0cdebdd84313d4f82
Juan A. Diaz <juan@linux.org.ar>
2013-08-21 18:45:12 -0500

    Implementación inicial de la clase vehiculo.
    - Esta implementación solo contienen los campos básicos.

---
+++ src/base.py
@@ -0,0 +1,10 @@
+# -*- coding:utf8 -*-
+
+class Vehiculo(object):
+    """ implementación básica de la clase vehiculos.
+    """
+
+    def __init__(self, anio, placa):
+        self.anio = anio
+        self.placa = placa
+
